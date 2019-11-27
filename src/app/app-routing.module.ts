d8817d82d79435ffc5423608fdafb9e88d4687eb
Loc <efdesigner@hotmail.com>
2019-11-27 11:29:29 -0300

    Criado módulo de catalogo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'catalogo', loadChildren: () => import('./catalogo/catalogo.module').then(m => m.CatalogoModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
