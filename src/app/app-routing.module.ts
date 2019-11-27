f620efdbd3dcab4143ce0444f8f4443c431bd78b
Loc <efdesigner@hotmail.com>
2019-11-27 13:46:38 -0300

    adicionado home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [{ path: 'catalogo', loadChildren: () => import('./catalogo/catalogo.module').then(m => m.CatalogoModule) }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'catalogo', loadChildren: () => import('./catalogo/catalogo.module').then(m => m.CatalogoModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
